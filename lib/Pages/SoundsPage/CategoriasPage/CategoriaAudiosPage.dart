5707e01839007eb66e3c3ec4d3cf2ffba69eafd2
LucasAmbesi <lucasambesi@hotmail.com>
2021-04-21 20:32:10 -0300

    Se agrega paramasProvider a barra superior de CategoriaAudios

--- lib/Pages/SoundsPage/CategoriasPage/CategoriaAudiosPage.dart
+++ lib/Pages/SoundsPage/CategoriasPage/CategoriaAudiosPage.dart
@@ -1,8 +1,11 @@
+import 'package:botonera_app/Helpers/Helpers.dart';
+import 'package:botonera_app/models/ParametrosProvider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:botonera_app/Pages/SoundsPage/AudioCard/FBAudiosCards.dart';
 import 'package:botonera_app/db/DAOs/AudioDAO.dart';
 import 'package:botonera_app/models/Categoria.dart';
+import 'package:provider/provider.dart';
 
 class CategoriaAudiosPage extends StatefulWidget {
   final Categoria categoria;
@@ -19,13 +22,25 @@ class _CategoriaAudiosPageState extends State<CategoriaAudiosPage> {
 
   @override
   Widget build(BuildContext context) {
+    var paramsProvider = Provider.of<ParametrosProvider>(
+      context,
+      listen: false,
+    );
+    Color colorFondo =
+        Helpers.getColorByParam(paramsProvider.colorBarraSuperior);
+    Color colorFondoContraste =
+        Helpers.getColorConstrastByParam(paramsProvider.colorBarraSuperior);
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(color: colorFondoContraste),
         title: Text(
           categoria.nombre,
+          style: TextStyle(
+            color: colorFondoContraste,
+          ),
         ),
-        backgroundColor: Colors.yellow[300],
-        shadowColor: Colors.white,
+        backgroundColor: colorFondo,
+        shadowColor: colorFondo,
       ),
       body: FutureBuilderCards(
         callback: AudioDAO.getAudiosByCategoria,
